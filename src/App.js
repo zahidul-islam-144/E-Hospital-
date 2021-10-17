d017355be9a942dfec85129d0a281a97c1fd7ff2
Zahidul Islam <zahidul.islam1@northsouth.edu>
2021-10-18 02:08:31 +0600

    ass-10 project set up done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App" class="p-6 items-center justify-center">
+        <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
+        <p class="tracking-widest">This is my first React App.</p>
+    </div>
+  );
+}
+
+export default App;
